53e4f6b8e6aed9383a1173854d877c041a7951a6
EdsonGP723 <edsongp1007@outlook.com>
2024-04-19 13:39:58 -0600

    Práctica 4

---
+++ Assets/MultijugadorEnRed/Scripts/MyNetworkManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class MyNetworkManager : NetworkManager
+{
+	public override void OnStartServer()
+	{
+		//base.OnStartServer();
+		Debug.Log("Servidor iniciado");
+	}
+	
+	public override void OnStopServer()
+	{
+		//base.OnStopServer();
+		Debug.Log("Servidor detenido");
+	}
+	
+	public override void OnClientConnect()
+	{
+		base.OnClientConnect();
+		Debug.Log("Cliente Conectado");
+	}
+	
+	public override void OnClientDisconnect()
+	{
+		base.OnClientDisconnect();
+		Debug.Log("Cliente desconectado desde el servidor");
+	}
+	
+	
+}
