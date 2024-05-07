45f4b1f2326b189e171e4ec54ebd5235d6ca82d9
EdsonGP723 <95706015+EdsonGP723@users.noreply.github.com>
2024-05-07 16:30:16 -0600

    avance de lobby

---
+++ Assets/Prácticas/Lobby/Scripts/JoinLobbyMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class JoinLobbyMenu : MonoBehaviour
+{
+	[SerializeField] private NetworkManagerLobby networkManager = null;
+	
+	[Header("UI")]
+	[SerializeField] private GameObject landindPagePanel = null;
+	[SerializeField] private TMP_InputField ipAdressInputField = null;
+	[SerializeField] private Button joinButton = null;
+	
+	private void OnEnable()
+	{
+		NetworkManagerLobby.OnClientConnected += HandleClientConnected;
+		NetworkManagerLobby.OnClientDisconnected += HandleClientDisconnected;
+	}
+	
+	private void HandleClientConnected()
+	{
+		joinButton.interactable = true;
+		gameObject.SetActive(false);
+		landindPagePanel.SetActive(false);
+	}
+	
+	private void HandleClientDisconnected()
+	{
+		joinButton.interactable = true;
+	}
+	
+	private void OnDesable()
+	{
+		NetworkManagerLobby.OnClientConnected -= HandleClientConnected;
+		NetworkManagerLobby.OnClientDisconnected -= HandleClientDisconnected;
+	}
+	
+	public void JoinLobby()
+	{
+		string ipAdress = ipAdressInputField.text;
+		networkManager.networkAddress = ipAdress;
+		networkManager.StartClient();
+		joinButton.interactable = false;
+	}
+}
---
+++ Assets/Prácticas/Lobby/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MainMenu : MonoBehaviour
+{
+	[SerializeField] private NetworkManagerLobby networkManager = null;
+	
+	[Header("UI")]
+	[SerializeField] private GameObject landinfPagePanel = null;
+	
+	public void HostLobby()
+	{
+		networkManager.StartHost();
+		landinfPagePanel.SetActive(false);
+	}
+}
